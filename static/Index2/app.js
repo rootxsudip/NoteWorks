ef3fca30bb2a0a794f94b20699ac92465880ce3f
Kollishion <126138338+Kollishion@users.noreply.github.com>
2023-08-22 09:59:48 +0530

    Update app.js

--- static/Index2/app.js
+++ static/Index2/app.js
@@ -19,3 +19,28 @@ btnPopup.onclick = () => {
 iconClose.onclick = () => {
   wrapper.classList.remove("active-popup");
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const agreeCheckbox = document.getElementById("agreeCheckbox");
+  const termsBox = document.getElementById("termsBox");
+  const acceptTerm = document.getElementById("acceptTerm");
+  const declineTerm = document.getElementById("declineTerm")
+  // agreeCheckbox.addEventListener("change", function () {
+  //   showTermsButton.disabled = !agreeCheckbox.checked;
+  // });
+
+  agreeCheckbox.addEventListener("click", function () {
+    termsBox.classList.remove("hidden");
+  });
+
+  acceptTerm.addEventListener("click", function () {
+    termsBox.classList.add("hidden");
+  });
+
+  declineTerm.addEventListener("click", function () {
+    agreeCheckbox.checked = false; // Uncheck the checkbox
+    termsBox.classList.add("hidden");
+  });
+
+
+});
