448247390f967c9b47665b1c984f37dcad2f3a19
Kollishion <126138338+Kollishion@users.noreply.github.com>
2023-11-02 18:25:17 +0530

    Adding notes webpage
    Webpage where users will write their notes

--- static/Notes/notes.js
+++ static/Notes/notes.js
@@ -1,42 +1,65 @@
 const sidebar = document.querySelector(".sidebar");
-const arrow_out = document.querySelector(".arrow-out");
-const arrow_in = document.querySelector(".arrow-in");
 const addNote = document.querySelector(".addNote");
 const textarea = document.querySelector(".user_text");
 const save = document.querySelector(".save");
 const dropbtn = document.querySelector(".profile");
 const dropdown = document.querySelector(".info") 
 const closeBtn = document.querySelector(".close-btn");
+let notes = document.querySelector(".notes");
+const container = document.querySelector(".container");
+const popup = document.querySelector(".popup");
+const addTitle = document.querySelector(".addTitle");
+const closePopup = document.querySelector(".close-btn2");
+let titleInput = document.querySelector("#title");
+let del = document.createElement("i");
+let textzone = document.createElement("textarea");
 
 dropbtn.addEventListener("click", function(){
-    dropdown.style.opacity = 1;
+    dropdown.style.display = "flex";
 })
 
 closeBtn.addEventListener("click", function(){
-    dropdown.style.opacity = 0;
-})
-
-window.addEventListener("DOMContentLoaded", () => {
-arrow_out.addEventListener("click", function(){
-    sidebar.style.opacity = 1;
-    sidebar.classList.toggle("isHidden");
-    arrow_out.style.opacity = 0;
-    arrow_in.style.opacity = 1;
-})
-
-arrow_in.addEventListener("click", function(){
-    sidebar.style.opacity = 0;
-    arrow_in.style.opacity = 0;
-    arrow_out.style.opacity = 1;
-    sidebar.classList.toggle("isHidden2");
-})
+    dropdown.style.display = "none";
 })
 
 addNote.addEventListener("click", function(){
-    textarea.textContent = "Type Here:"
+    popup.style.display = "block";
 })
 
-addFolder.addEventListener("click", function(){
-    sidebar.textContent = "saved"
+addTitle.addEventListener("click", ()=>{
+    if(titleInput.value!==""){
+    popup.style.display = "none";
+    textzone.setAttribute("contenteditable", "true");
+    textzone.className = "textzone";
+    container.appendChild(textzone);
+    let a = document.createElement('a');
+    let link = document.createTextNode(`${titleInput.value}`);
+    a.appendChild(link);
+    a.href = `notes.html#${titleInput.value}`;
+    del.className = "ri-close-circle-line crossNote";
+    notes.appendChild(a).appendChild(del);
+    if(del.addEventListener("click", removeNote)){
+    function removeNote(e){
+            e.target.parentElement.remove();
+            updateStorage();
+            }
+        }
+    }
 })
 
+function updateStorage() {
+  localStorage.setItem("textzone", textarea.innerHTML);
+}
+
+closePopup.addEventListener("click", () => {
+    popup.style.display = "none";
+})
+
+notes.addEventListener("click", myFunction);
+function myFunction(e) {
+		var elems = document.querySelector(".active");
+    if(elems !=null) {
+      elems.classList.remove("active");
+    }
+    e.target.className = "active";
+	}
